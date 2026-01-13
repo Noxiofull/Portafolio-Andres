32c44855f62bc880e7fd4bbd4df8a37da6c74a30
Noxiofull <anfeguar159@gmail.com>
2026-01-13 12:13:51 -0500

    Probando

--- src/components/componentsPlus/ShowCardAboutMe.jsx
+++ src/components/componentsPlus/ShowCardAboutMe.jsx
@@ -2,7 +2,7 @@ import { CardAboutMe } from "./cardAboutMe"
 import {CodeBracketIcon,UsersIcon,BookOpenIcon} from  "@heroicons/react/24/outline"
 
 const data = [
-    {text:"Proyectos Completados",icon:CodeBracketIcon,number:5},
+    {text:"Proyectos Completados",icon:CodeBracketIcon,number:4},
     {text:"Años de experiencia",icon:UsersIcon,number:1},
     {text:"Certificaciones",icon:BookOpenIcon,number:4},
 
